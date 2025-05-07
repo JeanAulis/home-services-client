c7d6bb8a1092c27fdd6c804b34d7cb88d8854ad4
moon <112243222+JeanAulis@users.noreply.github.com>
2025-05-08 00:22:24 +0800

    重写

--- booking-api/src/main/java/org/example/bookingapi/repository/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.bookingapi.repository;
-
-import org.example.bookingapi.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-    UserInfo findByUserPhoneNum(String userPhoneNum);
-} 
