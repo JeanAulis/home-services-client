8af9808aa23e62a43b60fd6fe90c1a8aa64bbb6c
moon <112243222+JeanAulis@users.noreply.github.com>
2025-05-08 19:47:32 +0800

    前端更改

---
+++ booking-frontend/pages/footer/footer.js
@@ -0,0 +1,16 @@
+Component({
+    properties: {
+      avatarUrl: {
+        type: String,
+        value: ''
+      },
+      name: {
+        type: String,
+        value: ''
+      },
+      studentId: {
+        type: String,
+        value: ''
+      }
+    }
+  }) 
